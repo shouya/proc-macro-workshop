12d129bff9e7ff10be26c4869e26d40519b713a8
shouya <526598+shouya@users.noreply.github.com>
2024-05-01 20:34:29 +0900

    remove main.rs content

--- main.rs
+++ main.rs
@@ -6,19 +6,4 @@
 // To run the code:
 //     $ cargo run
 
-use bitfield::*;
-
-#[bitfield]
-pub struct RedirectionTableEntry {
-  #[bits = 9]
-  trigger_mode: TriggerMode,
-  reserved: B7,
-}
-
-#[derive(BitfieldSpecifier, Debug)]
-pub enum TriggerMode {
-  Edge = 0,
-  Level = 1,
-}
-
 fn main() {}
